1b552bc67aab26543f4d2a55e1a607911ed46b45
Kevin Manning <kpm6411@gmail.com>
2019-05-02 14:11:40 -0400

    Added contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,50 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class Contact extends Component {
   render() {
     return(
-      <div><h1>Contact Page</h1></div>
+      <div className='contact-body'>
+        <Grid className='contact-grid'>
+          <Cell col={6}>
+            <h2>Kevin Manning</h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{ height: '250px' }}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'sans-serif' }}>
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    Bryan Cranston
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'sans-serif' }}>
+                    <i className="fa fa-skype" aria-hidden="true" />
+                    Aaron Paul
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{ fontSize: '30px', fontFamily: 'sans-serif' }}>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    Bob Odenkirk
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
     )
   }
 }
